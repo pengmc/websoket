848be2b4c0bfa6a602c655dce63aa3bc5f59c566
彭民材 <717372061@qq.com>
2022-08-25 10:28:02 +0800

    2

--- app.js
+++ app.js
@@ -2,22 +2,26 @@ const WebSocket = require("ws");
 const Koa = require("koa");
 const app = new Koa();
 
-const wss = new WebSocket.Server({ port: 8088 }); // websocket的端口
+// const wss = new WebSocket.Server({ port: 8088 }); // websocket的端口
 
-wss.on("connection", function connection(ws) {
-  ws.send("ws");
-  ws.on("message", function incoming(message) {
-    console.log(message.toString());
-    // 广播给所有用户
-    wss.clients.forEach(function each(client) {
-      if (client.readyState === WebSocket.OPEN) {
-        const obj = {};
-        obj.date = new Date();
-        obj.msg = message.toString();
-        client.send(JSON.stringify(obj));
-      }
-    });
-  });
+// wss.on("connection", function connection(ws) {
+//   ws.send("ws");
+//   ws.on("message", function incoming(message) {
+//     console.log(message.toString());
+//     // 广播给所有用户
+//     wss.clients.forEach(function each(client) {
+//       if (client.readyState === WebSocket.OPEN) {
+//         const obj = {};
+//         obj.date = new Date();
+//         obj.msg = message.toString();
+//         client.send(JSON.stringify(obj));
+//       }
+//     });
+//   });
+// });
+
+app.use(async (ctx) => {
+  ctx.body = "Hello Vercel";
 });
 
 app.listen(3001);
