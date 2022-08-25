5787ec38f6b2f68e6a72be304b69a933ab314411
彭民材 <717372061@qq.com>
2022-08-25 16:00:13 +0800

    1

--- app.js
+++ app.js
@@ -2,26 +2,25 @@ const WebSocket = require("ws");
 const express = require("express");
 const app = new express();
 
-// const wss = new WebSocket.Server({ port: 8088 }); // websocket的端口
+const wss = new WebSocket.Server({ port: 8088 }); // websocket的端口
 
-// wss.on("connection", function connection(ws) {
-//   ws.send("ws");
-//   ws.on("message", function incoming(message) {
-//     console.log(message.toString());
-//     // 广播给所有用户
-//     wss.clients.forEach(function each(client) {
-//       if (client.readyState === WebSocket.OPEN) {
-//         const obj = {};
-//         obj.date = new Date();
-//         obj.msg = message.toString();
-//         client.send(JSON.stringify(obj));
-//       }
-//     });
-//   });
-// });
+wss.on("connection", function connection(ws) {
+  ws.send("ws");
+  ws.on("message", function incoming(message) {
+    console.log(message.toString());
+    // 广播给所有用户
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        const obj = {};
+        obj.date = new Date();
+        obj.msg = message.toString();
+        client.send(JSON.stringify(obj));
+      }
+    });
+  });
+});
 
 app.get("/login", (req, res) => {
-  console.log(1231);
   res.send("hello world!");
 });
 
